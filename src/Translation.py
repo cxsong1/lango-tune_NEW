e387dee51e4dbfb973e061e29282303599ea0049
cxsong1 <csca12.cs@gmail.com>
2019-11-30 13:46:18 -0800

    Deleted the test lyrics

--- src/Translation.py
+++ src/Translation.py
@@ -13,7 +13,7 @@ def translate(lyrics, destination_lang):
     translator = Translator()
     translated = translator.translate(lyrics, dest=destination_lang)
     return translated.text
-""""
+"""
 Breaks down the lyrics of a song into individual lines to be translated, then concatenates all the
 translated strings to produce the translated lyrics of the entire song. 
 
@@ -29,77 +29,9 @@ def linebreak(lyrics):
     while i <len(lyrics):
         endIndex = i
         if lyrics[i] == '\n' or i == (len(lyrics)-1):
-            line = translate(lyrics[startIndex:endIndex+1], 'fr')
+            line = translate(lyrics[startIndex:endIndex+1], 'en')
             #line.replace('\n', '')
             translatedLyrics = translatedLyrics + ' ' + line
             startIndex = i+1
         i += 1
     return translatedLyrics
-
-
-linebreak("""I love it when you call me señorita
-I wish I could pretend I didn't need ya
-But every touch is ooh-la-la-la
-It's true, la-la-la
-Ooh, I should be runnin'
-Ooh, you keep me coming for ya
-
-
-Land in Miami
-The air was hot from summer rain
-Sweat drippin' off me
-Before I even knew her name, la-la-la
-It felt like ooh-la-la-la, yeah, no
-Sapphire moonlight
-We danced for hours in the sand
-Tequila Sunrise
-Her body fit right in my hands, la-la-la
-It felt like ooh-la-la-la, yeah
-
-
-I love it when you call me señorita
-I wish I could pretend I didn't need ya
-But every touch is ooh-la-la-la
-It's true, la-la-la
-Ooh, I should be runnin'
-Ooh, you know I love it when you call me señorita
-I wish it wasn't so damn hard to leave ya
-But every touch is ooh-la-la-la
-It's true, la-la-la
-Ooh, I should be runnin'
-Ooh, you keep me coming for ya
-
-
-Locked in the hotel
-There's just some things that never change
-You say we're just friends
-But friends don't know the way you taste, la-la-la
-'Cause you know it's been a long time coming
-Don't ya let me fall, oh
-Ooh, when your lips undress me, hooked on your tongue
-Ooh, love, your kiss is deadly, don't stop
-
-
-I love it when you call me señorita
-I wish I could pretend I didn't need ya
-But every touch is ooh-la-la-la
-It's true, la-la-la
-Ooh, I should be runnin'
-Ooh, you know I love it when you call me señorita
-I wish it wasn't so damn hard to leave ya
-But every touch is ooh-la-la-la
-It's true, la-la-la
-Ooh, I should be runnin'
-Ooh, you keep me coming for ya
-
-
-All along, I've been coming for ya
-And I hope it meant something to you
-Call my name, I'll be coming for ya
-Coming for ya
-For ya
-For ya
-For ya
-Ooh, I should be runnin'
-Ooh, you keep me coming for ya""")
-
