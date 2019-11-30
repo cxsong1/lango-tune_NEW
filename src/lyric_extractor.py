183303c3ea9809523415698c9a4507811cd027c0
Emily G <eagubski@gmail.com>
2019-11-30 13:40:16 -0800

    Update lyric_extractor.py
    pretty

--- src/lyric_extractor.py
+++ src/lyric_extractor.py
@@ -1,26 +1,12 @@
-import speech_recognition as sr
-from pydub import AudioSegment
-
 from lyrics_extractor import Song_Lyrics
 import re
 
 
-
-##
-##sound_file = "song1_4.wav"
-##
-### use sound_file as source
-##r = sr.Recognizer()
-##with sr.AudioFile(sound_file) as source:
-##    audio = r.record(source)  # read the entire audio file
-##
-##
-##print("Transcription: " + r.recognize_google(audio))
-##
-
-
 def song_lyric_extract(songtitle):
-    
+    """
+    Takes in a song name as a string, searches through
+    cutom engine and produces the lyrics as a string. 
+    """
 
     extract_lyrics = Song_Lyrics("AIzaSyCtjCkXwCKC5ZPMSVk6UMvBC4kbsm-QaSE",
                                  "003304024142726018998:qpokd7ayxyi" )
@@ -32,4 +18,5 @@ def song_lyric_extract(songtitle):
     return parsed_song
 
 
-
+#example call
+#print(song_lyric_extract("despacito"))
