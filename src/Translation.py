cd5f297dc7b0bfdeaf23655de6e990c376cedaa2
cxsong1 <csca12.cs@gmail.com>
2019-11-30 16:22:18 -0800

    Passed language argument to linebreak method. Also wrote code for song recognition

--- src/Translation.py
+++ src/Translation.py
@@ -5,7 +5,7 @@ Translates a line of song lyrics using the googletrans API
 
 @:param 
     lyrics: the current line of lyrics to translate
-    destination_lang: the language to be translated to 
+    destination_lang: the language we want to translate to 
 @:return
     the translated line of lyrics
 """
@@ -19,17 +19,18 @@ translated strings to produce the translated lyrics of the entire song.
 
 @:param 
     lyrics: all the lyrics in a string from the song that we wish to translate
+    destination_lang: the language we want to translate to
 @:return
     string with translated lyrics for the entire song
 """
-def linebreak(lyrics):
+def linebreak(lyrics, destination_lang):
     translatedLyrics = ''
     i = 0
     startIndex = 0
     while i <len(lyrics):
         endIndex = i
         if lyrics[i] == '\n' or i == (len(lyrics)-1):
-            line = translate(lyrics[startIndex:endIndex+1], 'en')
+            line = translate(lyrics[startIndex:endIndex+1], destination_lang)
             #line.replace('\n', '')
             translatedLyrics = translatedLyrics + ' ' + line
             startIndex = i+1
